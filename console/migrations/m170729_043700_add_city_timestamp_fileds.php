02201b0dbf6aaf19e4fa5cf02ed51034749bbc17
malikghanim <malik@mxd3d.com>
2017-08-21 16:08:44 +0000

    migrations and packages

--- console/migrations/m170729_043700_add_city_timestamp_fileds.php
+++ console/migrations/m170729_043700_add_city_timestamp_fileds.php
@@ -19,13 +19,13 @@ class m170729_043700_add_city_timestamp_fileds extends Migration
     // Use up()/down() to run migration code without a transaction.
     public function up()
     {
-        $this->addColumn('City', 'created_at', $this->integer());
+        //$this->addColumn('City', 'created_at', $this->integer());
         $this->addColumn('City', 'updated_at', $this->integer());
     }
 
     public function down()
     {
-        $this->dropColumn('City', 'created_at');
-        $this->dropColumn('City', 'created_at');
+        $this->dropColumn('City', 'updated_at');
+        //$this->dropColumn('City', 'created_at');
     }
 }
