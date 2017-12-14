3c5bf76fd37faea724a0abedc16846a925c0d336
malikghanim <malik@mxd3d.com>
2017-12-14 00:19:50 +0000

    bug fixing

--- common/models/Job.php
+++ common/models/Job.php
@@ -225,6 +225,10 @@ class Job extends \yii\db\ActiveRecord
                 'end_date' => $this->fav_end_date
             ]);
 
+            if ($fav->active == 3) {
+                var_dump('XXXXX');die;
+            }
+
             if (!empty($fav) && $fav->active != 3) {
                 $fav->active = 3;
                 $fav->save();
